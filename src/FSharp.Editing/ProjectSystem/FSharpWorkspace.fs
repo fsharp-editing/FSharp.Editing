f829e1135f95108a1c38b2275311ca9e7e1c9a61
Jared Hester <jared.hester@gmail.com>
2017-02-26 09:55:14 -0500

    combine buffermanager into FSharpWorkspace

--- src/FSharp.Editing/ProjectSystem/FSharpWorkspace.fs
+++ src/FSharp.Editing/ProjectSystem/FSharpWorkspace.fs
@@ -17,47 +17,132 @@ open FSharp.Editing
 
 type LastWriteTime = DateTime
 
-[<Export; Shared>]
-type FSharpWorkspace (hostServices : Host.HostServices) as self =
-    inherit Workspace (hostServices,Constants.FSharpLanguageName)
-    let bufferManager = new BufferManager(self)
-    let disposables = ResizeArray<IDisposable>()
-
-    let workspaceEvent = Event<_>() 
-
-    do  self.WorkspaceChanged.Subscribe (fun eventArgs -> workspaceEvent.Trigger eventArgs)
-        |> disposables.Add
-
-    let eventStream = workspaceEvent.Publish
-
-    let _checkProjectId (projectId:ProjectId) =
-        match self.CurrentSolution.ContainsProject projectId with
-        | false -> failwithf "The workspace does not contain a project with the id '%s'" ^ string projectId.Id
-        | true -> ()
-
-    let _projectOptions = ConcurrentDictionary<ProjectId,FSharpProjectOptions>()
-
-    let documentTimeStamps = ConcurrentDictionary<DocumentId, LastWriteTime>()
-
     // TODO -
     //  What additional data caches does the workspace need?
     //  * FSharpProjectOptions dictionary that updates on projectInfo change events?
     //  *
 
-    do  disposables.Add bufferManager
 
-    new () = new FSharpWorkspace(Host.Mef.DesktopMefHostServices.DefaultServices)
-
-//    new (aggregator:HostServicesAggregator) =
-//        new FSharpWorkspace (aggregator.CreateHostServices())
+//    let _checkProjectId (projectId:ProjectId) =
+//        match self.CurrentSolution.ContainsProject projectId with
+//        | false -> failwithf "The workspace does not contain a project with the id '%s'" ^ string projectId.Id
+//        | true -> ()
 //
-//    new () =
-//        new FSharpWorkspace(FSharpHostService())
+//    let _projectOptions = ConcurrentDictionary<ProjectId,FSharpProjectOptions>()
+
+[<Export; Shared>]
+type FSharpWorkspace (hostServices:Host.HostServices) as self =
+    inherit Workspace (hostServices,Constants.FSharpLanguageName)
+    let subscriptions = ResizeArray<IDisposable> ()
+    let workspaceChange = Event<_> () 
+    let eventStream = workspaceChange.Publish
+    let documentTimeStamps = ConcurrentDictionary<DocumentId, LastWriteTime> ()
+    let transientDocuments = ConcurrentDictionary<FilePath, DocumentId list> (StringComparer.OrdinalIgnoreCase)
+    let transientDocumentIds = HashSet<DocumentId> ()
+    let lockObj = obj ()
+
+    do 
+        eventStream.Subscribe self.OnWorkspaceChanged |> subscriptions.Add
+
+    let tryAddTransientDocument (fileName:string) (fileContent : string) =
+        if String.IsNullOrWhiteSpace fileName then false else
+        let projects = self.CurrentSolution.Projects
+        if projects.Count () = 0 then false else
+        let sourceText = SourceText.From fileContent
+        let documents : DocumentInfo list =
+            (projects, []) ||> Seq.foldBack ^ fun project docs ->
+                let docId = DocumentId.CreateNewId project.Id
+                let version = VersionStamp.Create ()
+                let docInfo =
+                    DocumentInfo.Create
+                        (docId, fileName, filePath = fileName,
+                        loader = TextLoader.From (TextAndVersion.Create(sourceText, version)))
+                docInfo :: docs
+        lock lockObj ^ fun () ->
+            let docIds = documents |> List.map ^ fun doc -> doc.Id
+            transientDocuments.TryAdd (fileName, docIds) |> ignore
+            transientDocumentIds.UnionWith docIds
+        documents |> List.iter ^ fun doc -> self.AddDocument doc |> ignore
+        true
+
+    new () = new FSharpWorkspace (Host.Mef.DesktopMefHostServices.DefaultServices)
+
+//    new (aggregator:HostServicesAggregator) = new FSharpWorkspace (aggregator.CreateHostServices())
+//    new () = new FSharpWorkspace(FSharpHostService())
 
     member __.WorkspaceEvents = eventStream
 
-    //let activeDocuments = HashSet<DocumentId>()
-    //new() = new FSharpWorkspace(HostServicesAggregator(Seq.empty))
+    member self.ApplyTextChanges (docId:DocumentId) (textChanges:LinePositionSpanTextChange seq) = async {
+        if Seq.isEmpty textChanges then return () else
+        match self.CurrentSolution.TryGetDocument docId with
+        | Some doc ->
+            let! (sourceText:SourceText) = doc.GetTextAsync ()
+            let sourceText =
+                (sourceText, textChanges) ||> Seq.fold ^ fun sourceText change ->
+                    let startOffset =
+                        sourceText.Lines.GetPosition ^ LinePosition (change.StartLine, change.StartLine)
+                    let endOffset =
+                        sourceText.Lines.GetPosition ^ LinePosition (change.EndLine, change.EndColumn)
+                    sourceText.WithChanges [| 
+                        TextChange (TextSpan (startOffset, endOffset - startOffset), change.NewText) 
+                    |]
+            self.OnDocumentChanged (docId, sourceText)
+        | None -> ()
+    }
+
+    member self.UpdateBuffer (bufferChange:BufferUpdate) = async {
+        if isNull bufferChange.FileName then () else
+        let documentIds = self.CurrentSolution.GetDocumentIdsWithFilePath bufferChange.FileName
+        if not documentIds.IsEmpty then
+            for docId in documentIds do
+                let doc = self.CurrentSolution.GetDocument docId
+                let! sourceText = doc.GetTextAsync () |> Async.AwaitTask
+                let startOffset =
+                    sourceText.Lines.GetPosition ^ LinePosition (bufferChange.StartLine, bufferChange.StartLine)
+                let endOffset = sourceText.Lines.GetPosition ^ LinePosition (bufferChange.EndLine, bufferChange.EndColumn)
+                let sourceText =
+                    sourceText.WithChanges [| 
+                        TextChange (TextSpan (startOffset, endOffset - startOffset), bufferChange.NewText) 
+                    |]
+                self.OnDocumentChanged (docId, sourceText)
+        else
+            tryAddTransientDocument bufferChange.FileName bufferChange.NewText |> ignore
+    }
+
+    member self.OnWorkspaceChanged (args:WorkspaceChangeEventArgs) =
+        let filename =
+            match args.Kind with
+            | WorkspaceChangeKind.DocumentAdded   -> (args.NewSolution.GetDocument args.DocumentId).FilePath
+            | WorkspaceChangeKind.DocumentRemoved -> (args.OldSolution.GetDocument args.DocumentId).FilePath
+            | _ -> String.Empty
+        if String.IsNullOrEmpty filename then ()
+        else
+        lock lockObj ^ fun () ->
+            match Dict.tryFind filename transientDocuments with
+            | None -> ()
+            | Some docIds ->
+                transientDocuments.TryRemove filename |> ignore
+                for docId in docIds do
+                    self.RemoveDocument docId |> ignore
+                    transientDocumentIds.Remove docId |> ignore
+
+
+    member self.FindProjectsByFileName (fileName:string) =
+        let dirInfo = (FileInfo fileName).Directory
+        let candidates =
+            self.CurrentSolution.Projects
+                .GroupBy(fun project -> (FileInfo project.FilePath).Directory.FullName)
+                .ToDictionary
+                    (   (fun grouping -> grouping.Key)
+                    ,   (fun grouping -> List.ofSeq grouping)
+                    )
+        let rec loop (dirInfo:DirectoryInfo) =
+            if isNull dirInfo then [] else
+            match candidates.TryGetValue dirInfo.FullName with
+            | true, ps -> ps
+            | _, _ -> loop dirInfo.Parent
+        loop dirInfo :> _ seq
+
     override __.CanOpenDocuments = true
     override __.CanApplyChange _ = true
 
@@ -200,118 +285,7 @@ type FSharpWorkspace (hostServices : Host.HostServices) as self =
 
 
     interface IDisposable with
-        member __.Dispose() =
-            disposables |> Seq.iter dispose
-            disposables.Clear()
-
-/// Stores a representation of the dirty buffers and temporary buffers within the editor
-and internal BufferManager (workspace:FSharpWorkspace) as self =
-    let transientDocuments = ConcurrentDictionary<FilePath, DocumentId list>(StringComparer.OrdinalIgnoreCase)
-    let transientDocumentIds = HashSet<DocumentId>()
-    let lockObj = obj()
-    let subscriptions = ResizeArray<IDisposable>()
-    do
-        workspace.WorkspaceEvents.Subscribe self.OnWorkspaceChanged |> subscriptions.Add
-
-    let tryAddTransientDocument (fileName : string) (fileContent : string) =
-        if String.IsNullOrWhiteSpace fileName then false
-        else
-        let projects = workspace.CurrentSolution.Projects
-        if projects.Count () = 0 then false
-        else
-        let sourceText = SourceText.From fileContent
-
-        let documents : DocumentInfo list =
-            (projects, []) ||> Seq.foldBack ^ fun project docs ->
-                let docId = DocumentId.CreateNewId project.Id
-                let version = VersionStamp.Create ()
-                let docInfo =
-                    DocumentInfo.Create
-                        (docId, fileName, filePath = fileName,
-                        loader = TextLoader.From (TextAndVersion.Create(sourceText, version)))
-                docInfo :: docs
-        lock lockObj ^ fun () ->
-            let docIds = documents |> List.map ^ fun doc -> doc.Id
-            transientDocuments.TryAdd (fileName, docIds) |> ignore
-            transientDocumentIds.UnionWith docIds
-        documents |> List.iter ^ fun doc -> workspace.AddDocument doc |> ignore
-        true
-
-    member __.ApplyTextChanges (docId:DocumentId) (textChanges:LinePositionSpanTextChange seq) = async {
-        if Seq.isEmpty textChanges then return () else
-        match workspace.CurrentSolution.TryGetDocument docId with
-        | Some doc ->
-            let! (sourceText:SourceText) = doc.GetTextAsync ()
-            let sourceText =
-                (sourceText, textChanges) ||> Seq.fold ^ fun sourceText change ->
-                    let startOffset =
-                        sourceText.Lines.GetPosition ^ LinePosition (change.StartLine, change.StartLine)
-                    let endOffset =
-                        sourceText.Lines.GetPosition ^ LinePosition (change.EndLine, change.EndColumn)
-                    sourceText.WithChanges [| 
-                        TextChange (TextSpan (startOffset, endOffset - startOffset), change.NewText) 
-                    |]
-            workspace.OnDocumentChanged (docId, sourceText)
-        | None -> ()
-    }
-
-    member __.UpdateBuffer (bufferChange:BufferUpdate) = async {
-        if isNull bufferChange.FileName then () else
-        let documentIds = workspace.CurrentSolution.GetDocumentIdsWithFilePath bufferChange.FileName
-        if not documentIds.IsEmpty then
-            for docId in documentIds do
-                let doc = workspace.CurrentSolution.GetDocument docId
-                let! sourceText = doc.GetTextAsync () |> Async.AwaitTask
-                let startOffset =
-                    sourceText.Lines.GetPosition ^ LinePosition (bufferChange.StartLine, bufferChange.StartLine)
-                let endOffset = sourceText.Lines.GetPosition ^ LinePosition (bufferChange.EndLine, bufferChange.EndColumn)
-                let sourceText =
-                    sourceText.WithChanges [| 
-                        TextChange (TextSpan (startOffset, endOffset - startOffset), bufferChange.NewText) 
-                    |]
-                workspace.OnDocumentChanged (docId, sourceText)
-        else
-            tryAddTransientDocument bufferChange.FileName bufferChange.NewText |> ignore
-    }
-
-    // TODO - This probaly needs to be expanded substantially to account for more workspace events
-    member __.OnWorkspaceChanged (args:WorkspaceChangeEventArgs) =
-        let filename =
-            match args.Kind with
-            | WorkspaceChangeKind.DocumentAdded   -> (args.NewSolution.GetDocument args.DocumentId).FilePath
-            | WorkspaceChangeKind.DocumentRemoved -> (args.OldSolution.GetDocument args.DocumentId).FilePath
-            | _ -> String.Empty
-        if String.IsNullOrEmpty filename then ()
-        else
-        lock lockObj ^ fun () ->
-            match Dict.tryFind filename transientDocuments with
-            | None -> ()
-            | Some docIds ->
-                transientDocuments.TryRemove filename |> ignore
-                for docId in docIds do
-                    workspace.RemoveDocument docId |> ignore
-                    transientDocumentIds.Remove docId |> ignore
-
-
-    member self.FindProjectsByFileName (fileName:string) =
-        let dirInfo = (FileInfo fileName).Directory
-        let candidates =
-            workspace.CurrentSolution.Projects
-                .GroupBy(fun project -> (FileInfo project.FilePath).Directory.FullName)
-                .ToDictionary
-                    (   (fun grouping -> grouping.Key)
-                    ,   (fun grouping -> List.ofSeq grouping)
-                    )
-        let rec loop (dirInfo:DirectoryInfo) =
-            if isNull dirInfo then [] else
-            match candidates.TryGetValue dirInfo.FullName with
-            | true, ps -> ps
-            | _, _ -> loop dirInfo.Parent
-        loop dirInfo :> _ seq
-
-
-    interface IDisposable with
-        member __.Dispose() =
+        member __.Dispose () =
             subscriptions |> Seq.iter dispose
             subscriptions.Clear ()
 
